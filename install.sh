9098e4336d0045f4d566b7c5500adb675464599d
klaake <kevin.laake@gmail.com>
2023-08-25 09:47:55 -0600

    made an install file and updated git ignore

---
+++ install.sh
@@ -0,0 +1,7 @@
+/usr/local/bin/python3 -m venv venv
+source venv/bin/activate
+pip install --upgrade pip
+pip install --upgrade niceGUI
+pip install --upgrade pandas
+pip install --upgrade matplotlib
+pip install --upgrade plotly
